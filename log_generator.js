bb427dfa1d8713629518f0bb4e2d319b9c38416d
Ashu Singh <94642928+Ashu-14-oct@users.noreply.github.com>
2025-08-20 16:56:02 +0530

    UPD: Changed DB name

--- log_generator.js
+++ log_generator.js
@@ -2,7 +2,7 @@ const { MongoClient } = require("mongodb");
 const fs = require("fs");
 const path = require("path");
 
-const uri = "mongodb://127.0.0.1:27017/Ashu";
+const uri = "mongodb://127.0.0.1:27017/ondc-db";
 const dbName = "ondcFISBuyer";
 const collectionName = "logs";
 
